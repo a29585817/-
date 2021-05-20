81e1f9fd82a34875326414c2084a78d157d7e854
Meng Chien <a0919149590@gmail.com>
2021-05-20 19:54:24 +0800

    first commit

---
+++ fourth-2.py
@@ -0,0 +1,58 @@
+import os  # operating system
+
+#讀取檔案
+def read_file(filename):
+    products = []       
+    with open (filename , "r",encoding="utf-8") as f:
+        for line in f:
+            if "商品,價格" in line:
+                continue
+            name , price =line.strip().split(",")
+            products.append([name,price])
+    print(products)
+    
+
+    return products
+
+
+
+#讓使用者輸入 建立記帳簿
+def user_input(products):
+    while True:
+        name = input("請輸入商品名稱:")
+        if name == "q" :
+            break
+        
+        price = input("請輸入商品價格")
+        p = [name,price] 
+        
+        products.append(p)
+        
+    print(products)
+    return products
+
+#印出所有商品購買紀錄
+def print_products(products):
+    for p in products:
+        print(p)
+
+#寫入檔案
+def write_file( filename , products ):
+    with open( filename ,"w",encoding="utf-8") as f:
+        f.write("商品,價格\n")
+        for p in products:
+            f.write(p[0]+","+p[1]+"\n")
+
+
+def main():
+    filename="product.csv"
+    if os.path.isfile(filename):   #確認檔案
+        print("yeah!找到檔案")
+        products = read_file(filename)
+    else:
+        print("找不到檔案")
+    products = user_input(products)
+    print_products(products)
+    write_file("product.csv",products)
+
+main()
